9bb5784ca9870cddb1f430652d84d8e5afb51a90
VitorHugoFMartins <vitorferreira010vhfm@gmail.com>
2024-10-08 14:54:17 -0300

    initial project

---
+++ Dominio/DTOs/LoginDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace minimal_api.Dominio.DTOs
+{
+    public class LoginDTO
+    {
+    }
+}
---
+++ Infraestrutura/DataBase/AppDataContext.cs
@@ -0,0 +1,6 @@
+﻿namespace minimal_api.Infraestrutura.DataBase
+{
+    public class AppDataContext
+    {
+    }
+}
